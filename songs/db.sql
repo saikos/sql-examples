1948731f659f1ba0ec18ba6bc2d7347855505112
Kostas Saidis <saikos@users.noreply.github.com>
2019-05-13 20:48:22 +0300

    Update db.sql

--- songs/db.sql
+++ songs/db.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `song_list` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `song_list`;
 -- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
 --
--- Host: localhost    Database: songs
+-- Host: localhost    Database: song_list
 -- ------------------------------------------------------
 -- Server version	5.6.24-log
 
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-13 20:36:45
+-- Dump completed on 2019-05-13 20:47:09
